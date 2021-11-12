34173ccdf5e071a861a9a9cf93fc0c0c096706f3
Ivancaicedo <ingivancaicedo@hotmail.com>
2021-11-12 00:08:30 -0500

    Se agrego el splashscreen

--- app/src/main/java/com/example/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+
+        setTheme(R.style.Theme_Proyecto)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }

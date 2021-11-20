e28c54d118bad9cdc7c4fc3ba8758c984ef6f150
Anderson Charry <Charry072013@gmail.com>
2021-11-19 20:56:21 -0500

    Agrege maps a Hobbiton

--- app/src/main/java/com/example/start/MainActivity2.kt
+++ app/src/main/java/com/example/start/MainActivity2.kt
@@ -6,8 +6,17 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() , OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -16,5 +25,25 @@ class MainActivity2 : AppCompatActivity() {
         imageButton4.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+        createFragment()
+    }
+
+    private fun createFragment() {
+            val mapFragment=supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+    private fun createMarker(){
+        val coordenadas = LatLng(-37.85783013651075, 175.68048688969265)
+        val marker = MarkerOptions().position(coordenadas)
+        map.addMarker(marker)
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordenadas,17f),2000,null)
+
     }
 }
+
+
